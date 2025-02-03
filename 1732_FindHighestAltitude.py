8a324a2102427da2cb769bc8200386e088a7c843
Paul-p03 <PPatullo03@gmail.com>
2025-02-02 23:36:14 -0500

    Added main, reformatted program and cleaned up comments

--- 1732_FindHighestAltitude.py
+++ 1732_FindHighestAltitude.py
@@ -4,8 +4,12 @@ class Solution(object):
         max_inp = 0
         alt = 0
 
-        for i in range(inp):
+        for i in range(inp):   #Finds highest sequential summation point
             alt += gain[i]
             max_inp = max(alt, max_inp)
 
         return max_inp 
+
+solution = Solution()
+array_input = solution.largestAltitude([-5,1,5,0,-7])
+print(f'Highest Altitude reached during road trip: {array_input}')
